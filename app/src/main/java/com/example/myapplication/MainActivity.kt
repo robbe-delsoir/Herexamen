6555c20f1aa68e0ef0b9168c626745d6d9c55b8b
robbe.delsoir <robbe.delsoir@student.vives.be>
2022-01-17 01:21:40 +0100

    1 ding terug

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 
 import androidx.appcompat.app.AppCompatActivity
@@ -15,18 +17,28 @@ import java.lang.StringBuilder
 const val Base_Url = "https://min-api.cryptocompare.com/"
 class MainActivity : AppCompatActivity() {
 
-
-    private lateinit var txtId: TextView
+    private lateinit var txtBTC: TextView
+    private lateinit var txtEUR: TextView
+    private lateinit var txtUSD: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         getMyData();
 
-        txtId = findViewById(R.id.txtId)
+        val secondActButton = findViewById<Button>(R.id.buttonAbout)
+        secondActButton.setOnClickListener{
+            val intent = Intent(this, activity_about::class.java)
+            startActivity(intent)
+        }
+
+        txtUSD = findViewById(R.id.txtUSD)
+        txtEUR = findViewById(R.id.txtEUR)
+        txtBTC = findViewById(R.id.txtBTC)
 
 
     }
+
 
     private fun getMyData() {
         val retrofitBuilder = Retrofit.Builder()
@@ -41,12 +53,32 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call<MyData?>, response: Response<MyData?>) {
 
                 val responseBody = response.body()
+                val responseBodyTwo = response.body()
+                val responseBodyThree = response.body()
+
                 val myStringBuilder = StringBuilder()
                 if (responseBody != null) {
                     myStringBuilder.append(responseBody.BTC)
+
                 }
 
-                txtId.text = myStringBuilder
+                val myStringBuilder2 = StringBuilder()
+                if (responseBody != null) {
+                    if (responseBodyTwo != null) {
+                        myStringBuilder.append(responseBodyTwo.EUR)
+                    }
+                }
+
+                val myStringBuilder3 = StringBuilder()
+                if (responseBody != null) {
+                    if (responseBodyThree != null) {
+                        myStringBuilder.append(responseBodyThree.USD)
+                    }
+                }
+
+                txtBTC.text = myStringBuilder
+                txtEUR.text = myStringBuilder2
+                txtUSD.text = myStringBuilder3
 
             }
 
